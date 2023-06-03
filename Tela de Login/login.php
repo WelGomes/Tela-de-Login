bd559a8bedf9d8daff9532ec1cc847c6dc01c87e
Welbert Gomes <92159326+WelGomes@users.noreply.github.com>
2023-06-03 15:33:05 -0300

    login.php

--- Tela de Login/login.php
+++ Tela de Login/login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="styleLogin.css">
+    <link rel="shortcut icon" href="login.jpg" type="image/x-icon">
 </head>
     <title>Tela de Login</title>
 </head>
@@ -25,7 +26,7 @@
               <div class="row">
                 <div class="col">
                   <div class="form-floating mb-3 ">
-                    <input type="email" class="form-control border-black p-2 mb-2 shadow-none" name="email" id="email"  placeholder="nome@exemplo.com">
+                    <input type="email" class="form-control is-invalid border-black p-2 mb-2 shadow-none" name="email" id="email"  placeholder="nome@exemplo.com">
                     <label for="email">Endereço de email</label>
                   </div>
                 </div>
@@ -33,7 +34,7 @@
               <div class="row">
                 <div class="col">
                   <div class="form-floating">
-                    <input type="password" class="form-control border-black p-2 mb-2 shadow-none" name="senha" id="senha" placeholder="senha">
+                    <input type="password" class="form-control is-invalid border-black p-2 mb-2 shadow-none" name="senha" id="senha" placeholder="senha">
                     <label for="senha">Senha</label>
                   </div>
                 </div>
